27b8f84247eb0d7ac64dba549798343eb23dd44c
thinhthp <thinhthp1@gmail.com>
2025-09-29 20:35:05 +0700

    feat(auth): add email confirmation and password reset via SendGrid

---
+++ EngConnect/EngConnect.Services/DTOs/Account/ResetPasswordRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngConnect.Services.DTOs.Account
+{
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public string? UserId { get; set; }
+
+        [Required]
+        public string? Token { get; set; }
+
+        [Required]
+        public string? NewPassword { get; set; }
+    }
+}
